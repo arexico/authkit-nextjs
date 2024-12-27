38d1d9934bf5788e980f38e95ad98643e379857b
icrayix <icrayix@gmail.com>
2024-12-27 13:57:14 +0100

    Apply patches

--- src/get-authorization-url.ts
+++ src/get-authorization-url.ts
@@ -1,11 +1,15 @@
 import { workos } from './workos.js';
-import { WORKOS_CLIENT_ID, WORKOS_REDIRECT_URI } from './env-variables.js';
+import { WORKOS_CLIENT_ID, WORKOS_REDIRECT_URI, WORKOS_AUTHKIT_DOMAIN_OVERRIDE } from './env-variables.js';
 import { GetAuthURLOptions } from './interfaces.js';
 import { headers } from 'next/headers';
 
 async function getAuthorizationUrl(options: GetAuthURLOptions = {}) {
   const headersList = await headers();
   const { returnPathname, screenHint, organizationId, redirectUri = headersList.get('x-redirect-uri') } = options;
+
+  if (WORKOS_AUTHKIT_DOMAIN_OVERRIDE) {
+    return `${WORKOS_AUTHKIT_DOMAIN_OVERRIDE}/${screenHint === 'sign-up' ? 'sign-up' : ''}?client_id=${WORKOS_CLIENT_ID}&redirect_uri=${redirectUri ?? WORKOS_REDIRECT_URI}&state=${returnPathname ? btoa(JSON.stringify({ returnPathname })) : ''}&response_type=code`;
+  }
 
   return workos.userManagement.getAuthorizationUrl({
     provider: 'authkit',
